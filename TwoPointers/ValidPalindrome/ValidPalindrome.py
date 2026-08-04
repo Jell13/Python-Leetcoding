06b368971629245cfb2fceb17ab790bd97f249a4
Jason Sugiharto <jasonliesugiharto@gmail.com>
2026-08-04 15:29:07 -0700

    solved the valid palindrome question

--- TwoPointers/ValidPalindrome/ValidPalindrome.py
+++ TwoPointers/ValidPalindrome/ValidPalindrome.py
@@ -1,2 +1,21 @@
 def validPalindrome(s):
-    pass
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        if alphaNum(s[l]) and alphaNum(s[r]):
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+        elif alphaNum(s[l]) == False:
+            l += 1
+        elif alphaNum(s[r]) == False:
+            r -= 1
+
+    return True
+
+def alphaNum(c):
+    return ((ord('A') <= ord(c) <= ord('Z')) or
+            (ord('a') <= ord(c) <= ord('z')) or
+            (ord('0') <= ord(c) <= ord('9')))
