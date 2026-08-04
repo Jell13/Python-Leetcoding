8b797e294b18be3fa61e576b9243ef8330439f65
Jason Sugiharto <jasonliesugiharto@gmail.com>
2026-08-04 14:09:21 -0700

    solved longest sequence

--- Arrays&Hash/LongestSequence/LongestSequence.py
+++ Arrays&Hash/LongestSequence/LongestSequence.py
@@ -1,3 +1,18 @@
 def longestConsecutive(nums):
-    pass
+
+    sNums = set(nums)
+    counter = 0
+    for num in sNums:
+        count = 0
+        if (num - 1) in sNums:
+            continue
+        count += 1
+        while (num + count) in sNums:
+            count += 1
+
+        counter = max(counter, count)
+
+    return counter
+
+print(longestConsecutive([2,20,4,10,3,4,5]))
 
